4bcff63e7fd79a232cb39b5a6643805c929cd1cb
chldnrwo <chldnrwo777@naver.com>
2023-01-12 16:29:30 +0900

    first commit

---
+++ src/main/java/com/dadaek/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.dadaek.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Trips1Application.class);
+	}
+
+}
---
+++ src/main/java/com/dadaek/demo/Trips1Application.java
@@ -0,0 +1,13 @@
+package com.dadaek.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trips1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trips1Application.class, args);
+	}
+
+}
